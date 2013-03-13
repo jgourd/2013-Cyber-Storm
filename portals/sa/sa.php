6ddaa8f31c1da6ba2c48d6fa805972a09f73b7ea
jgourd <disintuitive@gmail.com>
2013-03-12 20:39:52 -0500

    modified the sa portal

--- portals/sa/sa.php
+++ portals/sa/sa.php
@@ -19,53 +19,13 @@
 	</head>
 
 	<body>
-		<div id="sa_row">
-			<div id="quadrant">
-				<div id="title" class="cs">2013 Cyber Storm</div>
-			</div>
-			<div id="quadrant">
-				<div id="title" class="green">GREEN</div>
-				<div class="nodes" id="green"></div>
-			</div>
-			<div id="quadrant">
-				<div id="title" class="yellow">YELLOW</div>
-				<div class="nodes" id="yellow"></div>
-			</div>
-			<div id="quadrant"><br/></div>
-		</div>
-		<div id="sa_row">
-			<div id="quadrant">
-				<div id="title" class="blue">BLUE</div>
-				<div class="nodes" id="blue"></div>
-			</div>
-			<div id="quadrant" class="attack top">
-				<br/>
-			</div>
-			<div id="quadrant">
-				<div id="title" class="purple">PURPLE</div>
-				<div class="nodes" id="purple"></div>
-			</div>
-		</div>
-		<div id="sa_row">
-			<div id="quadrant">
-				<div id="title" class="red">RED</div>
-				<div class="nodes" id="red"></div>
-			</div>
-			<div id="quadrant" class="attack bottom">
-				<br/>
-			</div>
-			<div id="quadrant">
-				<div id="title" class="orange">ORANGE</div>
-				<div class="nodes" id="orange"></div>
-			</div>
-		</div>
-		<div id="sa_row">
-			<div id="quadrant"><br/></div>
-			<div id="quadrant" class="other">
-				<div id="other"></div>
-				<div id="title" class="other">OTHER</div>
-			</div>
-			<div id="quadrant"><br/></div>
-		</div>
+		<div id="info"></div>
+		<div id="red"></div>
+		<div id="blue"></div>
+		<div id="green"></div>
+		<div id="yellow"></div>
+		<div id="purple"></div>
+		<div id="orange"></div>
+		<div id="other"></div>
 	</body>
 </html>
