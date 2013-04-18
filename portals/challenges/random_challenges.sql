64c023e37623239ce2815344387edd5a8472a726
root <root@scott-VGN-NW350F.(none)>
2013-04-17 22:26:08 -0500

    added winner logos. update script to have more test records.
    what's left :
    upgrade roll-over text

--- portals/challenges/random_challenges.sql
+++ portals/challenges/random_challenges.sql
@@ -2,6 +2,13 @@ insert into challenges (name,description,points,timestamp_start,winner_id) value
 
 insert into challenges (name,description,points,timestamp_start,winner_id) values ("Hack the vm", "Hack blue team VM", 100, "00:00:00", null);
 
+
+
+insert into challenges (name,description,points,timestamp_start,winner_id) values ("Hack the wifi 2", "Hack wifi with ssd of hackMeNow2", 50, null, 1);
+insert into challenges (name,description,points,timestamp_start,winner_id) values ("Hack the wifi 2", "Hack wifi with ssd of hackMeNow2", 50, null, 2);
 insert into challenges (name,description,points,timestamp_start,winner_id) values ("Hack the wifi 2", "Hack wifi with ssd of hackMeNow2", 50, null, 3);
+insert into challenges (name,description,points,timestamp_start,winner_id) values ("Hack the wifi 2", "Hack wifi with ssd of hackMeNow2", 50, null, 4);
+insert into challenges (name,description,points,timestamp_start,winner_id) values ("Hack the wifi 2", "Hack wifi with ssd of hackMeNow2", 50, null, 5);
+insert into challenges (name,description,points,timestamp_start,winner_id) values ("Hack the wifi 2", "Hack wifi with ssd of hackMeNow2", 50, null, 6);
 
 insert into challenges (name,description,points,timestamp_start,winner_id) values ("Hack jean website", "replace jeans website with redirect to whitehouse.gov", 50, "00:00:00", null);
